ab6273259b123ce309329c6bfbb3b08ec56ee010
oplik0 <opliko@opliko.dev>
2025-03-30 23:54:10 +0200

    feat: add basic python interface, try with just lists

--- trans-engine/src/lib.rs
+++ trans-engine/src/lib.rs
@@ -1,14 +1,6 @@
-use arrow::pyarrow::ToPyArrow;
-use arrow_array::iterator::ArrayIter;
-use arrow_array::run_iterator::RunArrayIter;
-use arrow_array::types::Utf8Type;
-use arrow_array::{Array, ArrayRef, PrimitiveArray, StringArray};
-use arrow_schema::DataType;
+
 use parser::parse_line;
 use pyo3::prelude::*;
-use pyo3_arrow::error::PyArrowResult;
-use pyo3_arrow::PyArray;
-
 mod lang;
 mod parser;
 mod transforms;
@@ -16,17 +8,15 @@ mod transforms;
 // TODO: fix arrow typing
 
 #[pyfunction]
-pub fn run(py: Python, rules: PyArray, words: PyArray) -> PyArrowResult<PyObject> {
+pub fn run(rules: Vec<String>, words: Vec<String>) -> PyResult<Vec<String>> {
     // We can call py.allow_threads to ensure the GIL is released during our
     // operations
     // This example just wraps `arrow_select::take::take`
     let mut output_array: Vec<String> = Vec::new();
-    let rules_array = rules.to_arro3(py);
-    let words_array = words.to_arro3(py);
-    for element in rules_array.iter() {
+    for element in rules.iter() {
         match parse_line(&mut element.to_string().as_str()) {
             Ok(rule) => {
-                for values in words_array.iter() {
+                for values in words.iter() {
                     // perf issue: clone is expensive
                     match lang::Rule::run_all(rule.clone(), values.to_string()) {
                         Some(result) => output_array.push(result),
@@ -41,7 +31,7 @@ pub fn run(py: Python, rules: PyArray, words: PyArray) -> PyArrowResult<PyObject
     }
     
     // TODO: remove copying here too
-    Ok(StringArray::from(output_array).to_data().to_pyarrow(py)?)
+    Ok(output_array)
 }
 
 /// A Python module implemented in Rust. The name of this function must match
